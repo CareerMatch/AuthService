94fae9853db2e2975fcb84193693cdab0ad74b74
Bagus Nugra <bagusnugrahaxd@gmail.com>
2024-10-29 21:50:10 +0100

    Add required to some variables in Model

--- AuthServices/Models/AuthModel.cs
+++ AuthServices/Models/AuthModel.cs
@@ -3,23 +3,42 @@ namespace AuthServices.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 public class AuthModel
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; } = "User";
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; }
 
-        // Token storage fields
-        public string RefreshToken { get; set; }
-        public DateTime RefreshTokenExpiry { get; set; }
-    }
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required]
+    [MinLength(8)]
+    public string PasswordHash { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Role { get; set; } = "User";
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Token storage fields
+    public string RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiry { get; set; }
+}
+
 
